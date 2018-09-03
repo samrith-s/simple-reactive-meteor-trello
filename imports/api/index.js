6865f45553829a40e336f169acb0442f69925b15
Samrith Shankar <samrith.s@media.net>
2018-09-04 01:57:19 +0530

    Completely redid sequencing login on DnD

--- imports/api/index.js
+++ imports/api/index.js
@@ -5,26 +5,40 @@ export const Buckets = new Mongo.Collection('buckets');
 export const Cards = new Mongo.Collection('cards');
 
 Meteor.methods({
-    updateMultipleCards({
-        draggableId: _id,
-        destination: { droppableId: bucketId, index }
-    }) {
-        Cards.update(
-            { seq: { $eq: index }, seq: { $gt: index } },
-            {
-                $inc: { seq: 1 }
-            },
-            { multi: true }
-        );
+    updateMultipleCards(
+        cardId,
+        { droppableId: sourceBucketId, index: startIndex },
+        { droppableId: destinationBucketId, index: endIndex }
+    ) {
+        const cards = Cards.find(
+            { bucketId: destinationBucketId },
+            { sort: { seq: 1 } }
+        ).fetch();
+        const seq = cards[endIndex] ? cards[endIndex].seq : 0;
 
-        Cards.update(
-            { _id },
-            {
-                $set: {
-                    bucketId,
-                    seq: index
+        if (startIndex === endIndex && sourceBucketId === destinationBucketId) {
+            return;
+        }
+
+        if (sourceBucketId !== destinationBucketId) {
+            for (let i = endIndex, len = cards.length; i < len; i++) {
+                Cards.update({ _id: cards[i]._id }, { $inc: { seq: 1 } });
+            }
+        } else {
+            if (startIndex < endIndex) {
+                for (let i = startIndex + 1; i <= endIndex; i++) {
+                    Cards.update({ _id: cards[i]._id }, { $inc: { seq: -1 } });
+                }
+            } else {
+                for (let i = endIndex; i < startIndex; i++) {
+                    Cards.update({ _id: cards[i]._id }, { $inc: { seq: 1 } });
                 }
             }
+        }
+
+        Cards.update(
+            { _id: cardId, bucketId: sourceBucketId },
+            { $set: { seq, bucketId: destinationBucketId } }
         );
     },
 
