650f34c5e6210b17db9e34007c7c5a55e60e847e
Samrith Shankar <samrith.s@media.net>
2018-09-03 21:43:09 +0530

    Added method to update multiple cards

--- imports/api/index.js
+++ imports/api/index.js
@@ -1,4 +1,30 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Buckets = new Mongo.Collection('buckets');
 export const Cards = new Mongo.Collection('cards');
+
+Meteor.methods({
+    updateMultipleCards({
+        draggableId: _id,
+        destination: { droppableId: bucketId, index }
+    }) {
+        Cards.update(
+            { seq: { $eq: index }, seq: { $gt: index } },
+            {
+                $inc: { seq: 1 }
+            },
+            { multi: true }
+        );
+
+        Cards.update(
+            { _id },
+            {
+                $set: {
+                    bucketId,
+                    seq: index
+                }
+            }
+        );
+    }
+});
